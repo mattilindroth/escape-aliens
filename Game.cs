8989b1092ac69f223f6e6dde10cfa01c4645c2b6
Matti Lindroth <matti.lindroth@gmail.com>
2019-08-20 22:53:47 +0300

    Add functionality to Vector2d and some initial thoughts on Game class

--- Game.cs
+++ Game.cs
@@ -2,7 +2,21 @@ using System;
 using SDL2;
 namespace escape_aliens
 {
-    public class Game {
-        
+    public class Game 
+    {
+        private int _screenWidth;
+        private int _screenHeight;
+
+    	public Game(int initialScreenWidth = 1440, int initialScreenHeight = 900) 
+    	{
+			_screenHeight = initialScreenHeight;
+			_screenWidth = initialScreenWidth;
+    	}
+
+    	public void Run(float desiredFps) 
+    	{
+
+    	}
+
     }
 }
--- Vector2D.cs
+++ Vector2D.cs
@@ -4,5 +4,61 @@ namespace escape_aliens {
     public class Vector2D {
         public double X {get;set;}
         public double Y {get;set;} 
+
+        public static double operator * (Vector2D v1, Vector2D v2) {
+        	double result;
+
+        	result = v1.X * v2.X;
+        	result = result + (v1.Y * v2.Y);
+
+        	return result;
+        }
+
+        public static Vector2D operator *(double val, Vector2D v) {
+        	return new Vector2D
+        	{
+        		X = val * v.X,
+        		Y = val * v.Y,
+        	};
+        }
+
+        public static Vector2D operator *(Vector2D v, double val) {
+        	return new Vector2D
+        	{
+        		X = val * v.X,
+        		Y = val * v.Y,
+        	};
+        }
+
+        public static Vector2D operator -(Vector2D v1, Vector2D v2 ) 
+        {
+
+        	var result = new Vector2D
+        	{
+        		X = v1.X - v2.X, 
+        		Y = v1.Y - v2.Y
+        	};
+
+        	return result;
+        }
+        public static Vector2D operator -(Vector2D v) {
+        	return new Vector2D {
+        		X = -v.X,
+        		Y = v.Y,
+        	};
+        }
+
+        public static Vector2D operator +(Vector2D v1, Vector2D v2 ) 
+        {
+        	Vector2D result;
+
+        	result = new Vector2D
+        	{
+        		X = v1.X + v2.X, 
+        		Y = v1.Y + v2.Y
+        	};
+
+        	return result;
+        }
     }
 }
