b3bba90807750f900bcd6013590f73c447e318a9
Matti Lindroth <matti.lindroth@gmail.com>
2019-10-19 10:14:17 +0300

    Minor development to Mouse input handler while watching paw patrol

--- Engine/Input/MouseBindings.cs
+++ Engine/Input/MouseBindings.cs
@@ -6,5 +6,25 @@ namespace escape_aliens.Engine.Input
 {
     public delegate void MouseMove(int x, int y);
     public delegate void MouseButtonStatusChange( bool isDown );
+    public class MouseBindings {
+        private List<MouseMove> _mouseMoveReceivers;
 
+        public MouseBindings() {
+            _mouseMoveReceivers = new List<MouseMove>();
+        }
+
+        public void RegisterMouseMovementListener(MouseMove mouseMoveDelegate) {
+            _mouseMoveReceivers.Add(mouseMoveDelegate);
+        }
+
+        public void UnregisterMouseMovementListener(MouseMove mouseMoveDelegate) {
+            _mouseMoveReceivers.Remove(mouseMoveDelegate);
+        }
+
+        internal void DispatchMouseMovement(int x, int y) {
+            foreach(var receiver in _mouseMoveReceivers) {
+                receiver.Invoke(x,y);
+            }
+        }
+    }
 }
