95c2b7031410bd70535a7f86b09e34d8a83276d1
Matti Lindroth <Matti.K.Lindroth@aktia.fi>
2019-11-21 22:44:08 +0200

    something

--- Engine/MathExtra/Vector2D.cs
+++ Engine/MathExtra/Vector2D.cs
@@ -27,12 +27,18 @@ namespace escape_aliens.Engine.MathExtra {
         public double X {get;set;}
         public double Y {get;set;} 
 
-		public CrossProduct(Vector2D another) {
-
+		public double CrossProduct(Vector2D another) {
+			return X*another.Y - Y * another.X;
 		}
 
 		public double AngleWith(Vector2D another) {
-			
+			double len1 = Length;
+			double len2 = another.Length;
+			double denominator = len1 * len2;
+			if(denominator != 0) {
+				return Math.Acos((this * another)/denominator);
+			}
+			return double.NaN;
 		}
 
         public static double operator * (Vector2D v1, Vector2D v2) {
